56897dfb277bf769a794321a6391acbbfe26076e
Rajeev5024 <rajeevkumarranjan880@gmail.com>
2024-09-12 16:05:28 +0530

    written utils code and cors setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.db.js";
+import { app } from "./app.js";
+import { error } from "console";
 
 dotenv.config({
 	path:'./env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+	app.listen(process.env.PORT || 8000, ()=>{
+		console.log(`Server is runnugn at PORT: ${process.env.PORT}`)
+	})
+	app.on("error",(error)=>{
+		console.log("ERR while listening on port :", error);
+		throw error;
+	})
+})
+.catch((err)=>{
+	console.log("mongoDB connection failed: ",err);
+})
